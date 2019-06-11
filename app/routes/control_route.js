2b23eca6bfd1ebec356a671c920e66acb8d96439
root <sackboy11@gmail.com>
2019-06-11 03:15:30 +0000

    Everything working wel..

--- app/routes/control_route.js
+++ app/routes/control_route.js
@@ -2,6 +2,8 @@ module.exports = function(app){
     //type will be something like on, off, toggle
     //where zone will be the zone we want.
     app.get('/control/:type/:zone', (req, res)=>{
+        type = req.params.type;
+        zone = req.params.zone;
         res.send("Hello World");
     });
 }
