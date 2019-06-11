73c99fe8a0b46c540ec668962eddb581520dc23f
root <sackboy11@gmail.com>
2019-06-11 03:36:27 +0000

    Zone sanitizxe class works well enough.

---
+++ app/classes/zone_sanitize.js
@@ -0,0 +1,35 @@
+module.exports = {
+    sanitize: function(s){
+        //this class converts words in arrays to the corresponding sones
+        //TODO: Impliment this lol
+        //each zone has an array of valid words, '*' is special, and means it is included in sayin 'all'
+        s_cln = s.replace(/\s/g, '').toLowerCase(); //remove spaces, set lower
+        zone0 = ["0", "all"];
+        zone1 = ["1", "one", "bedroom", "thebedroom"];
+        zone2 = ["2", "two", "thestairs", "stairwell", "thestaiwell", "stairs"];
+        zone3 = ["3", "livingroom", "thelivingroom"];
+        zone4 = ["4"];
+
+        //TODO: Add multiples together by splitting on the 'AND'
+        zones = [];
+
+        if(zone0.includes(s_cln)){
+            zones.push(1);
+            zones.push(2);
+            zones.push(3);
+            zones.push(4);
+        }
+        if(zone1.includes(s_cln))
+            zones.push(1);
+        if(zone2.includes(s_cln))
+            zones.push(2);
+        if(zone3.includes(s_cln))
+            zones.push(3);
+        if(zone4.includes(s_cln))
+            zones.push(4);
+
+
+
+        return zones;
+    }
+}
--- app/routes/control_route.js
+++ app/routes/control_route.js
@@ -2,8 +2,9 @@ module.exports = function(app){
     //type will be something like on, off, toggle
     //where zone will be the zone we want.
     app.get('/control/:type/:zone', (req, res)=>{
+        var z_s = require('../classes/zone_sanitize');
         type = req.params.type;
         zone = req.params.zone;
-        res.send("Hello World");
+        res.send(z_s.sanitize(zone));
     });
 }
